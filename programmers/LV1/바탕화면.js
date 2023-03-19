c4e86c39358bc7f13ba58893385f18d731774513
sohee lee <kksm456@gmail.com>
2023-03-19 23:17:45 +0900

    LV1 바탕화면

---
+++ programmers/LV1/바탕화면.js
@@ -0,0 +1,15 @@
+function solution(wallpaper) {
+  let result = [];
+  let y = [];
+  let x = [];
+  let answer = wallpaper.map((d)=> [...d]);  
+  answer.map((d, i) => { d.map((v, i2) => { if (v == "#") { x.push(i2); y.push(i) } }) });
+
+  y.sort((a, b) => a - b);
+  x.sort((a, b) => a - b);
+    
+  result.push(y[0], x[0], y[y.length - 1] + 1, x[x.length - 1] + 1);
+  return result;
+    
+}
+
